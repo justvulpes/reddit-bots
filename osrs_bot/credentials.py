051f7f092e95714ce0e2364cf7e21edb03d3288d
Ragnar Rebase <rrebase@gmail.com>
2017-08-22 17:03:10 +0300

    update

--- osrs_bot/credentials.py
+++
@@ -1,9 +0,0 @@
-# reddit
-CLIENT_ID = "2DgPZNealyUnbw"
-CLIENT_SECRET = "x876AWqilHdB0pvIDS44mF75lpY"
-USERNAME = "osrs_pets"
-PASSWORD = "Rebase1996"
-USER_AGENT = "PRAW"
-
-# pastebin
-DEV_KEY = 'a555018ea077e6d083e1a6e2b5b7f317'
